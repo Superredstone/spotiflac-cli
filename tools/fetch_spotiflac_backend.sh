8e5b70473658cdb0d2ab3a34442157564d83ead1
Superredstone <patrickcanal3@gmail.com>
2026-02-12 16:50:44 +0100

    style(tools): remove dead code comments

--- tools/fetch_spotiflac_backend.sh
+++ tools/fetch_spotiflac_backend.sh
@@ -3,7 +3,6 @@
 set -e
 
 FOLDER=lib
-# USELESS_FILES="file_dialog.go folder.go"
 
 rm -rf lib/
 git clone https://github.com/afkarxyz/SpotiFLAC.git
@@ -14,12 +13,6 @@ rm -rf SpotiFLAC
 
 sed -i "s/package main/package app/g" app/app.go
 sed -i "s/\"spotiflac\/backend\"/backend \"github.com\/Superredstone\/spotiflac-cli\/lib\"/g" app/app.go
-# sed -i "s/backend./lib./g" app/app.go
-# sed -i "s/\"github.com\/wailsapp\/wails\/v2\/pkg\/runtime\"//g" $FOLDER/app.go
-
-# for i in $USELESS_FILES; do 
-	# rm -rf $FOLDER/$i
-# done
 
 for i in $(ls lib/); do
 	sed -i "s/package backend/package $FOLDER/g" $FOLDER/$i
